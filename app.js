696d0c0841ac8a5b3eb3480706bffa38a04d5f42
Ahmed Panju <ahmedpanju@Ahmeds-Laptop.local>
2023-01-25 10:58:47 +0800

    first' git push

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ limit: "5mb" }));
+app.use(helmet());
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+const github = require("./src/routes/github");
+
+app.use("/github", github);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
---
+++ src/routes/github.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const R = require("ramda");
+const router = express.Router();
+const { Octokit, App } = require("octokit");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_KEY,
+});
+
+router.post("/new-query", async (req, res) => {
+  try {
+    let finalUserList = [];
+    let finalUserListCsv = [
+      [
+        "Name",
+        "Username",
+        "Email",
+        "Bio",
+        "Hireable",
+        "Twitter",
+        "Location",
+        "Github",
+        "Company",
+        "Blog",
+      ],
+    ];
+    const users = await octokit.request(
+      "GET /search/users{?q,sort,order,per_page,page}",
+      {
+        q: req.body.query,
+        per_page: 100,
+        page: req.body.pageNumber,
+      }
+    );
+
+    await Promise.all(
+      users.data.items.map(async (user, index) => {
+        const singleUserData = await octokit.request("GET /user/{id}", {
+          id: user.id,
+        });
+
+        finalUserList.push({
+          name: R.pathOr("N/A", ["data", "name"], singleUserData),
+          username: R.pathOr("N/A", ["data", "login"], singleUserData),
+          email: R.pathOr("N/A", ["data", "email"], singleUserData),
+          bio: R.pathOr("N/A", ["data", "bio"], singleUserData),
+          hireable: R.path(["data", "hireable"], singleUserData) ? "Yes" : "No",
+          twitter: R.path(["data", "twitter_username"], singleUserData)
+            ? `https://www.twitter.com/${R.path(
+                ["data", "twitter_username"],
+                singleUserData
+              )}`
+            : "N/A",
+          githubProfile: R.pathOr("N/A", ["data", "html_url"], singleUserData),
+          location: R.pathOr("N/A", ["data", "location"], singleUserData),
+          company: R.pathOr("N/A", ["data", "company"], singleUserData),
+          blog: !!R.path(["data", "blog"], singleUserData)
+            ? R.path(["data", "blog"], singleUserData)
+            : "N/A",
+        });
+        finalUserListCsv.push([
+          R.pathOr("N/A", ["data", "name"], singleUserData),
+          R.pathOr("N/A", ["data", "login"], singleUserData),
+          R.pathOr("N/A", ["data", "email"], singleUserData),
+          R.pathOr("N/A", ["data", "bio"], singleUserData),
+          R.path(["data", "hireable"], singleUserData) ? "Yes" : "No",
+          R.path(["data", "twitter_username"], singleUserData)
+            ? `https://www.twitter.com/${R.path(
+                ["data", "twitter_username"],
+                singleUserData
+              )}`
+            : "N/A",
+          R.pathOr("N/A", ["data", "location"], singleUserData),
+          R.pathOr("N/A", ["data", "html_url"], singleUserData),
+          R.pathOr("N/A", ["data", "company"], singleUserData),
+          !!R.path(["data", "blog"], singleUserData)
+            ? R.path(["data", "blog"], singleUserData)
+            : "N/A",
+        ]);
+      })
+    );
+
+    res.status(200).json({
+      users: finalUserList,
+      csvUsers: finalUserListCsv,
+      totalCount: users.data.total_count,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+  }
+});
+
+module.exports = router;
